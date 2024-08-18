69ae24d3895fff695a08a3b4d0eb3822f6159991
EkremL <ekremlale1907@gmail.com>
2024-08-18 21:54:42 +0300

    file structure

---
+++ backend/src/index.ts
@@ -0,0 +1,30 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import "dotenv/config";
+import { log } from "console";
+import mongoose from "mongoose";
+
+async function connectToDatabase() {
+  try {
+    await mongoose.connect(process.env.MONGO_CONNECTION_STRING as string);
+    console.log("Successfully connected to MongoDB");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+  }
+}
+
+const port = 5000;
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+app.get("/api/test", async (req: Request, res: Response) => {
+  res.json({ message: "testing api endpoint, hello!" });
+});
+
+app.listen(port, () => {
+  log(`listening on port ${port}`);
+  connectToDatabase();
+});
